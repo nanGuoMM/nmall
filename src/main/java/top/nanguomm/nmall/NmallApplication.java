2b7ba39dd3100be9c216dc56b0e0a8a2bd89a05a
nanguo <zhuyuinss@gmail.com>
2024-12-04 17:38:26 +0800

    2024-12-4

---
+++ src/main/java/top/nanguomm/nmall/NmallApplication.java
@@ -0,0 +1,13 @@
+package top.nanguomm.nmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/nanguomm/nmall/NmallApplicationTests.java
@@ -0,0 +1,13 @@
+package top.nanguomm.nmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
